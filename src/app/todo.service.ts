084dc949bd23b56c872f33c37c76fa8b3bee5bd0
Kim Rohner <rohner.kim@gmail.com>
2019-11-23 16:45:25 +0100

    Upgrade angular material
    - fixed compile error with core.js

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -7,6 +7,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
+
 @Injectable({
   providedIn: 'root'
 })
